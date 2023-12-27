ef0e0c37c7ad119e5f853e48c92458474d7a1ebc
kiran <kirankhetariya@tjcg.in>
2023-12-27 16:49:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,12 +31,3 @@ console.log(typeof a);
 // Variable store string data type
 a = "GeeksforGeeks";
 console.log(typeof a);
-
- 
-// Variable store Boolean data type
-a = true;
-console.log(typeof a);
- 
-// Variable store undefined (no value) data type
-a = undefined;
-console.log(typeof a);
